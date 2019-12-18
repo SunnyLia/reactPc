cf0b4158c9c3958e052e3d1373ee7f8b74c9bf44
sunnyLia <1326155622@qq.com>
2019-12-18 14:25:13 +0800

    添加mock

---
+++ api/app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+
+const bodyParse = require('body-parser');
+// const cookieParser = require('cookie-parser');
+
+const Mock = require("./mock.min.js");
+const jsonData = require("./mock-data.js");
+
+// app.use(cookieParser());
+app.use(bodyParse.urlencoded({extended:false}));
+app.use(express.static('public'));
+
+// 解决跨域问题
+app.all('*',function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+
+  if (req.method == 'OPTIONS') {
+    res.send(200); /*让options请求快速返回*/
+  }
+  else {
+    next();
+  }
+})
+
+// mock 数据写法
+// 1、首页列表
+app.get('/homeLists', function (req, res) {
+  var result = {};
+  result = Mock.mock(jsonData.homeLists);
+  res.end(JSON.stringify(result));
+})
+
+// 监听3000端口
+const server = app.listen(8080, function () {
+	console.log('listening at =====> http://127.0.0.1:8080...');
+});
---
+++ api/mock-data.js
@@ -0,0 +1,13 @@
+/**
+ * @desc 假数据的模板
+ */
+
+module.exports = {
+	"data|14": [{
+		"title": "@cparagraph",
+		"img": "@image('200x100', '#4A7BF7', 'Hello')",
+		"price|1-5000": 1,
+		"number|0-1000": 1,
+		"fullRed|1": ["满99减20", "满199减50", "满299减100"]
+	}]
+}
